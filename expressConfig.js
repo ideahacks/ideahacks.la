b71ef1fbb4d1cff30c744f0cb41611624bdef6dd
Jeffrey Chan <jeffschan97@gmail.com>
2017-07-17 22:12:49 -0700

    Initial express app setup

---
+++ expressConfig.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+let app = express()
+
+app.set('port', (process.env.PORT || 3000))
+app.use(express.static(path.join(__dirname, 'views')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./expressConfig.js')
+
+app.listen(app.get('port'), () => {
+    console.log('Sever started on port: ', app.get('port'))
+})
