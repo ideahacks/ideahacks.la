43d845d4f34de1e28af51157dc83d945f84c5f27
Jeffrey Chan <jeffschan97@gmail.com>
2017-07-28 19:46:21 -0700

    hbs view engine solved everything

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const favicon = require('serve-favicon')
+const hbs = require('hbs')
 const ideahacks = require('./ideahacks')
 
 let app = express()
 
 app.set('port', (process.env.PORT || 3000))
+app.set('view engine', 'hbs')
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'views')))
