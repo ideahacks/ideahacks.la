b0c93e289ff97b87b71e33ca180f5fb1f768f3a3
Trey Crossley <treycrossley@gmail.com>
2017-08-12 12:27:57 +0200

    updated index.js

--- ideahacks/routes/api/index.js
+++ ideahacks/routes/api/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const apiRouter = express.Router()
 const apiHandlers = require('./team.js')
+const apiHandlers2 = require('./users.js')
 
 apiRouter.get('/team', apiHandlers.getTeam)
 apiRouter.post('/team', apiHandlers.postTeam)
 apiRouter.delete('/team', apiHandlers.deleteTeam)
 
-apiRouter.get('/users', apiHandlers.getUsers)
-apiRouter.post('/users', apiHandlers.postUsers)
-// apiRouter.delete('/users', apiHandlers.deleteUsers)
+apiRouter.get('/users', apiHandlers2.getUsers)
+apiRouter.post('/users', apiHandlers2.postUsers)
+// apiRouter.delete('/users', apiHandlers2.deleteUsers)
 
 module.exports = apiRouter
