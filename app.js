c0fe41bb812d209160092960b1e1fb1d4be5c35d
Jeffrey Chan <jeffschan97@gmail.com>
2017-08-19 23:10:11 -0700

    Pull from development and polish

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ideahacks = require('./ideahacks')
 
 let app = express()
 
-app.set('port', (process.env.PORT || 3000))
+app.set('port', process.env.PORT || 3000)
 app.set('view engine', 'hbs')
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
@@ -15,14 +15,10 @@ app.use(express.static(path.join(__dirname, 'views')))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(ideahacks.routes.mainRouter)
 app.use('/admin', ideahacks.routes.adminRouter)
 app.use('/api', ideahacks.routes.apiRouter)
-
-app.get('/team', (req, res) => {
-  res.render('team')
-})
 
 module.exports = app
