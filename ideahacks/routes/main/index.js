d7202c4dddbf6b90c7e30194105cf63a9095059b
Jeffrey Chan <jeffschan97@gmail.com>
2017-08-22 21:53:18 -0700

    Dashboard and application route handlers

--- ideahacks/routes/main/index.js
+++ ideahacks/routes/main/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mainRouter = express.Router()
 const staticHandlers = require('./static.js')
 const authHandlers = require('./auth.js')
+const dashboardHandlers = require('./dashboard.js')
 
 mainRouter.get('/', staticHandlers.getMain)
 
@@ -12,5 +13,9 @@ mainRouter.post('/login', authHandlers.postLogin)
 
 mainRouter.get('/registration', authHandlers.getRegistration)
 mainRouter.post('/registration', authHandlers.postRegistration)
+
+mainRouter.get('/dashboard', dashboardHandlers.getDashboard)
+
+mainRouter.get('/application', dashboardHandlers.getApplication)
 
 module.exports = mainRouter
