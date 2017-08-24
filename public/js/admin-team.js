d7da6c0b7661041b17c42c1db6b71a245552eef2
Jordan Yen <jordanyen726@gmail.com>
2017-08-24 11:39:59 -0700

    submit button works

--- public/js/admin-team.js
+++ public/js/admin-team.js
@@ -1,21 +1,19 @@
-var main = function(){
-    $(document).ready(() => {
-      $('form').submit(e => {
-        e.preventDefault()
+$(document).ready(() => {
+  $('form').submit(e => {
+    e.preventDefault()
 
-        let teamMembers = []
-        $('.member-email').each(function(i, el) {
-          teamMembers.push(el.value)
-        })
-
-        let teamData = {
-          teamName: $('.team-name').val(),
-          teamNumber: $('.team-number').val(),
-          members: teamMembers,
-          parts: []
-        }
-
-        $.ajax({ url: '/admin/teams', type: 'POST', data: teamData }).done()
-      })
+    let teamMembers = []
+    $('.member-email').each(function(i, el) {
+      teamMembers.push(el.value)
     })
-}
+
+    let teamData = {
+      teamName: $('.team-name').val(),
+      teamNumber: $('.team-number').val(),
+      members: teamMembers,
+      parts: []
+    }
+
+    $.ajax({ url: '/admin/teams', type: 'POST', data: teamData }).done()
+  })
+})
