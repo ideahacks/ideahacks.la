f7afff1b8da5ed3af6f402d00f7d49d2c94a736d
Jordan Yen <jordanyen726@gmail.com>
2017-08-24 22:20:06 -0700

    no repeats

--- ideahacks/routes/admin/team.js
+++ ideahacks/routes/admin/team.js
@@ -2,22 +2,33 @@ const Team = require('../../db').Team
 
 const getTeams = (req, res) => {
   Team.find({}).then(teams => {
-    console.log(teams)
+    teams.reverse()
     res.render('admin-team', { teams })
   })
 }
 
 const postTeams = (req, res) => {
-  console.log(req.body)
-  let newTeam = new Team({
-    teamName: req.body.teamName || '',
-    teamNumber: req.body.teamNumber || '',
-    members: [],
-    parts: []
-  })
-  newTeam.save()
+  Team.find({
+    $or: [{ teamName: req.body.teamName }, { teamNumber: req.body.teamNumber }]
+  }).then(team => {
+    if (team.length === 0) {
+      console.log('make new team')
+      let newTeam = new Team({
+        teamName: req.body.teamName || '',
+        teamNumber: req.body.teamNumber || '',
+        members: [],
+        parts: []
+      })
+      newTeam.save()
 
-  res.json({ message: 'post request received' })
+      return res.json({ message: 'new team has been added to the database' })
+    }
+    console.log('fuck you no repeats')
+    return res.json({
+      status: 'failure',
+      message: 'A team with this team name and number has already been created!'
+    })
+  })
 }
 
 const deleteTeams = (req, res) => {
