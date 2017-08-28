1110b6f9c6445c81f0a86cdb4e816d2925ba74ce
Jeffrey Chan <jeffschan97@gmail.com>
2017-08-27 21:58:41 -0700

    communications module

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 const app = require('./app.js')
 const httpServer = require('http').Server(app)
 const io = require('socket.io')(httpServer)
-
-io.of('/admin/teams').on('connection', (socket) => {
-  socket.on('team created', teamData => {
-    socket.broadcast.emit('team created', teamData)
-  })
-  socket.on('user typing', () => {
-    socket.broadcast.emit('user typing')
-  })
-  socket.on('no longer typing', () => {
-    socket.broadcast.emit('no longer typing')
-  })
-})
+const initializeSocketIO = require('./ideahacks').communication.initializeSocketIO(io)
 
 httpServer.listen(app.get('port'), () => {
   console.log('Server started on port: ', app.get('port'))
