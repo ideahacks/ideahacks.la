13afe7021e5ffb482060270a7b6cdfde9547407d
Jeffrey Chan <jeffschan97@gmail.com>
2017-08-29 01:09:13 -0700

    Callback hell to deal with all the error checking with mongo

--- ideahacks/routes/admin/team.js
+++ ideahacks/routes/admin/team.js
@@ -15,40 +15,52 @@ const postTeams = (req, res) => {
       message: 'Please fill out all fields of the form!'
     })
   }
-    console.log(req.body.members)
-  User.find({
-    email: { $in: [req.body.members] }
-  }).then(emails => {
-      console.log(emails)
-    if (emails.length === 0) {
-      return res.json({
-        status: 'failure',
-        message: 'A user you entered is not in our database!'
-      })
-    }
-  }) 
-  Team.find({
-    $or: [{ teamName: req.body.teamName }, { teamNumber: req.body.teamNumber }, {members: {$in:[req.body.members]}}]
-  }).then(team => {
-    if (team.length === 0) {
-      let newTeam = new Team({
-        teamName: req.body.teamName || '',
-        teamNumber: req.body.teamNumber || '',
-        members: [],
-        parts: []
-      })
-      newTeam.save()
 
-      return res.json({
-        status: 'success',
-        message: 'New team has been added to the database!'
-      })
-    }
-    return res.json({
-      status: 'failure',
-      message: 'A team with this name or number has already been created!'
-    })
+  User.find({
+    email: { $in: req.body.members }
   })
+    .then(users => {
+      let tempEmails = req.body.members
+      for (let user of users) {
+        let index = tempEmails.indexOf(user.email)
+        if (index !== -1) {
+          tempEmails.splice(index, 1)
+        }
+      }
+      if (tempEmails.length > 0) {
+        return res.json({
+          status: 'failure',
+          message: 'The emails ' + tempEmails.join(', ') + ' are not within our database!'
+        })
+      }
+
+      return Team.find({
+        $or: [{ teamName: req.body.teamName }, { teamNumber: req.body.teamNumber }]
+      })
+        .then(team => {
+          if (team.length !== 0) {
+            return res.json({
+              status: 'failure',
+              message: 'A team with this name or number has already been created!'
+            })
+          }
+
+          let newTeam = new Team({
+            teamName: req.body.teamName || '',
+            teamNumber: req.body.teamNumber || '',
+            members: [],
+            parts: []
+          })
+          newTeam.save()
+
+          return res.json({
+            status: 'success',
+            message: 'New team has been added to the database!'
+          })
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 }
 
 const deleteTeams = (req, res) => {
