795a342336edf2af045d67920515f7a93cee229a
Jeffrey Chan <jeffschan97@gmail.com>
2017-08-30 23:25:36 -0700

    Dashboard Router Adjusted front end input fields to have correct names. JS that takes form inputs and sends them to back end with AJAX Application post request handler that saves data into user

---
+++ public/js/application.js
@@ -0,0 +1,34 @@
+$(() => {
+  $('.application-form').submit(e => e.preventDefault())
+
+  $('.submit-application').click(() => getAndSendApplicationData())
+})
+
+function getAndSendApplicationData() {
+  let teammateEmails = []
+  $('.teammate-email').each((i, el) => {
+    if (el.value !== '') teammateEmails.push(el.value)
+  })
+
+  let applicationData = {
+    firstName: $('input[name="firstName"]').val(),
+    lastName: $('input[name="lastName"]').val(),
+    phone: $('input[name="phone"]').val(),
+    school: $('input[name="school"]').val(),
+    major: $('input[name="major"]').val(),
+    year: $('input[name="year"]').val(),
+    github: $('input[name="github"]').val(),
+    linkedin: $('input[name="linkedin"]').val(),
+    hasTeam: $('select[name="hasTeam"]').find(':selected').text(),
+    teammates: teammateEmails,
+    foodRestrictions: $('input[name="foodRestrictions"]').val(),
+    vehicleNeed: $('select[name="vehicleNeed"]').find(':selected').text(),
+    skillsAndExperience: $('textarea[name="skillsAndExperience"]').val(),
+    pastHackathonExperience: $('textarea[name="pastHackathonExperience"]').val(),
+    reasonForParticipation: $('textarea[name="reasonForParticipation"]').val(),
+    themeIdea: $('textarea[name="themeIdea"]').val(),
+    desiredParts: $('textarea[name="desiredParts"]').val()
+  }
+
+  $.ajax({ url: '/dashboard/application', type: 'POST', data: applicationData }).done()
+}
