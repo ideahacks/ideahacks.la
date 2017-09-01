d1b1981651825c08471c31010974abc0bf6d4775
Trey Crossley <treycrossley@gmail.com>
2017-09-01 14:40:46 -0700

    part async and verification

--- ideahacks/routes/admin/parts.js
+++ ideahacks/routes/admin/parts.js
@@ -7,7 +7,27 @@ const getParts = (req, res) => {
 }
 
 const postParts = (req, res) => {
-  let newPart = new Part({
+
+	//return failure if anything is wrong with the submitted part
+	if(req.body.partName === '' || !(Number.isInteger(req.body.stock) && req.body.stock >= 0) || req.body.description === '' || req.body.owners === undefined)
+	{
+		return res.json({ status: 'failure', message: 'Please fill out all fields of the form!'})
+	}
+ 	//end of form failure test
+
+
+	//Check if part name already exists in database
+	Part.find({ partName: req.body.partName}).then(parts =? {
+		if( parts.length > 0) {
+			return res.json({ status: 'failure', message: 'Part already exists'})	
+		}
+	})
+
+	//end of uniqueness check
+
+//if part passes both tests, then create the part
+
+let newPart = new Part({
     partName: req.body.partName || '',
     stock: req.body.stock || '',
     description: req.body.description || ' ',
@@ -15,8 +35,14 @@ const postParts = (req, res) => {
   })
   newPart.save()
 
-  res.json({ message: 'Received POST request' })
+  res.json({ message: 'Success! Part has been created' })
+//end of part creation
+	
+
+//end postParts function
 }
+
+
 
 const deleteParts = (req, res) => {
   Part.remove().then(err => {
--- public/js/admin-parts.js
+++ public/js/admin-parts.js
@@ -8,6 +8,32 @@ $(document).ready(() => {
       description: $('.description').val()
     }
 
-    $.ajax({ url: '/admin/parts', type: 'POST', data: partsData }).done()
+    $.ajax({ url: '/admin/parts', type: 'POST', data: partsData }).done(results => {
+    	if (results.status === 'failure') {
+	$('.error-message').text(results.message)
+	} 
+	else {
+	
+	
+		//appendTeam
+
+		let newPartHTML = [
+			'<li>',
+			'<h1>', partData.partName, '</h1>',
+			'<h1>', partData.stock, '</h1>',
+			'</li>'
+		]
+
+		newPartHTML = newPartHTML.join('')
+
+		$(newPartHTML).appendTo('.team-list')
+	
+
+		//clear form
+		$('input').val('')
+	}
+    
+    
+    })
   })
 })
