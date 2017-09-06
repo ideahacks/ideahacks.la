24870cbc31fd19e0d173ea777a229907903924bd
Jeffrey Chan <jeffschan97@gmail.com>
2017-09-05 18:14:20 -0700

    Polish

--- public/js/admin-parts.js
+++ public/js/admin-parts.js
@@ -9,37 +9,29 @@ $(document).ready(() => {
     }
 
     $.ajax({ url: '/admin/parts', type: 'POST', data: partsData }).done(results => {
-    	if (results.status === 'failure') {
-	$('.error-message').text(results.message)
-	} 
-	else {
-	
-		//appendPart
-		appendNewPart(partData)
+      if (results.status === 'failure') {
+        $('.error-message').text(results.message)
+      } else {
+        appendNewPart(partsData)
 
-
-		//clear form
-		$('input').val('')
-	}
-    
-    
+        $('input').val('')
+      }
     })
   })
+
+  $('input').focus(() => $('.error-message').text(''))
 })
 
 function appendNewPart(partData) {
+  // prettier-ignore
+  let newPartHTML = [
+    '<li>',
+      '<h1>', partData.partName, '</h1>',
+      '<h1>', partData.stock, '</h1>',
+    '</li>'
+  ]
 
-		
+  newPartHTML = newPartHTML.join('')
 
-		let newPartHTML = [
-			'<li>',
-			'<h1>', partData.partName, '</h1>',
-			'<h1>', partData.stock, '</h1>',
-			'</li>'
-		]
-
-		newPartHTML = newPartHTML.join('')
-
-		$(newPartHTML).appendTo('.part-list')
-	
+  $(newPartHTML).appendTo('.part-list')
 }
