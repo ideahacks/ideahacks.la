47064ec32bc2d2f42b149dca2c02360ace254abe
Jeffrey Chan <jeffschan97@gmail.com>
2017-09-12 17:45:35 -0700

    Redesigned application

--- ideahacks/routes/dashboard/application.js
+++ ideahacks/routes/dashboard/application.js
@@ -2,7 +2,7 @@ const formatUser = require('../../helpers').userFunctions.formatUser
 
 const getApplication = (req, res) => {
   req.user = formatUser(req.user)
-  res.render('application', { user: req.user })
+  res.render('dashboard-application', { user: req.user })
 }
 
 const postApplication = (req, res) => {
