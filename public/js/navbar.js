971106a61a935ed703392a32569384980835cb0e
sydney-ng <sydney.ng@yahoo.com>
2017-09-20 09:35:06 -0700

    enhancement/157-main-page-navigation: added scrolling

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -12,3 +12,21 @@ function toggleMobileNav() {
     document.getElementById(idText).style.height = 'auto'
   }
 }
+
+$(document).ready(function() {
+  $('a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault()
+      var hash = this.hash
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash
+        }
+      )
+    }
+  })
+})
