d74021bc53209ba9b72ca17f3cba33036743865b
sydney-ng <sydney.ng@yahoo.com>
2017-09-20 09:40:43 -0700

    enhancement/157-main-page-navigation: added scrolling to navbar-scroll.js

--- public/js/navbar-scroll.js
+++ public/js/navbar-scroll.js
@@ -11,3 +11,21 @@ $(document).ready(function() {
     }
   })
 })
+
+$(document).ready(function() {
+  $('a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault()
+      var hash = this.hash
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash
+        }
+      )
+    }
+  })
+})
