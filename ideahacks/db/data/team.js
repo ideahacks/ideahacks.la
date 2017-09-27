c2586149aaf68883bcfe32d164efa0683f05ec8a
Trey Crossley <treycrossley@gmail.com>
2017-09-27 15:25:50 -0700

    added extra properties for each teammember in template page

--- ideahacks/db/data/team.js
+++ ideahacks/db/data/team.js
@@ -4,18 +4,24 @@ module.exports = [
     teammates: [
       {
         name: 'Derrick Benson',
-        organization: 'Theta Tau'
-        //add other fields here
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/derrickjbenson/',
+        gitHub: 'https://github.com/derrickjbenson'
       },
       {
         name: 'Ted Akiyama',
-        organization: 'IEEE'
-        //add other fields here
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/tedakiyama',
+        gitHub: 'https://github.com/tedakiyama'
       },
       {
         name: 'Wenlong Xiong',
-        organization: 'IEEE'
-        //add other fields here
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/wenlongx',
+        gitHub: 'https://github.com/wenlongx'
       }
     ]
   },
@@ -24,19 +30,30 @@ module.exports = [
     teammates: [
       {
         name: 'Kenny Chan',
-        organization: 'IEEE'
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/kchan6/',
+        gitHub: 'https://github.com/kennych418'
       },
       {
         name: 'Mabel Chen',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'http://linkedin.com/in/mabel-chen'
       },
       {
         name: 'Frederick Kennedy',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/fkennedy0110',
+        gitHub: 'https://github.com/fkennedy',
+        website: 'http://fkennedy.co'
       },
       {
         name: 'Jingbin Huang',
-        organization: 'IEEE'
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'linkedin.com/in/jingbin-huang-77b3b0100'
       }
     ]
   },
@@ -45,11 +62,13 @@ module.exports = [
     teammates: [
       {
         name: 'Audrey Tu',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg'
       },
       {
         name: 'Jacob Ng',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg'
       }
     ]
   },
@@ -58,19 +77,27 @@ module.exports = [
     teammates: [
       {
         name: 'Samuel Chen',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/samueljchen'
       },
       {
         name: 'Tao Lu',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/tao-lu-4b4350130/'
       },
       {
         name: 'Christopher Wang',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/christopher-wang-7ba61213a'
       },
       {
         name: 'Jonathan Myong',
-        organization: 'IEEE'
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/jonathan-myong-831343121/'
       }
     ]
   },
@@ -79,31 +106,50 @@ module.exports = [
     teammates: [
       {
         name: 'Jeffrey Chan',
-        organization: 'IEEE'
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/jeffreyschan/',
+        gitHub: 'https://github.com/jeffreyxchan',
+        website: 'http://jeffreyxchan.me/'
       },
       {
         name: 'Jordan Yen',
-        organization: 'IEEE'
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/jordan-yen',
+        gitHub: 'https://github.com/jordanyenucla'
       },
       {
         name: 'Brittany Hsu',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        gitHub: 'https://github.com/hsubrittany'
       },
       {
         name: 'Patrick Shih',
-        organization: 'Theta Tau'
+        organization: 'Theta Tau',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/shihpatrick',
+        gitHub: 'https://github.com/shihpatrick'
       },
       {
         name: 'Trey Crossley',
-        organization: 'IEEE'
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        linkedIn: 'https://www.linkedin.com/in/sidney-trey-crossley-3384a7104',
+        gitHub: 'https://github.com/treycrossley'
       },
       {
         name: 'Sydney Ng',
-        organization: 'IEEE'
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        gitHub: 'https://github.com/sydney-ng'
       },
       {
         name: 'Haiying Huang',
-        organization: 'IEEE'
+        organization: 'IEEE',
+        picturePath: 'http://www.ideahacks.la/img/team/derrick-benson-old.jpg',
+        gitHub: 'https://github.com/arthurhaiying'
       }
     ]
   }
