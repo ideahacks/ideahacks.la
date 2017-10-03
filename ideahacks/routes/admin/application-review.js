2fb05a7c7fe11df20f505268a67b3d4499ba35a4
Jeffrey Chan <jeffschan97@gmail.com>
2017-10-02 21:54:07 -0700

    stats boxes styled and positioned

--- ideahacks/routes/admin/application-review.js
+++ ideahacks/routes/admin/application-review.js
@@ -2,7 +2,23 @@ const User = require('../../db').User
 
 const getApplicationReview = (req, res) => {
   User.find({ hasApplication: true }).then(users => {
-    res.render('admin-application-review.hbs', { applications: users })
+    let numberApplications = users.length
+    let numberAccepted = 0
+    let numberRejected = 0
+    let numberPending = 0
+    for (let user of users) {
+      if (user.applicationStatus === 'accepted') numberAccepted++
+      else if (user.applicationStatus === 'pending') numberPending++
+      else if (user.applicationStatus === 'rejected') numberRejected++
+    }
+
+    res.render('admin-application-review.hbs', {
+      applications: users,
+      numberApplications,
+      numberAccepted,
+      numberRejected,
+      numberPending
+    })
   })
 }
 
