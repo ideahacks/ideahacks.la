c0cc3f5114ec8840937373e6b74f466d19d13728
Jeffrey Chan <jeffschan97@gmail.com>
2017-10-25 23:52:02 -0700

    Production config fix

--- ideahacks/config/index.js
+++ ideahacks/config/index.js
@@ -1,6 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    dbURI: process.env.dbURI
+    dbURI: process.env.dbURI,
+    sessionSecret: process.env.sessionSecret,
+    host: process.env.host,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
   }
 } else {
   module.exports = require('./development.json')
