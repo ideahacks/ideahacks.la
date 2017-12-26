5307e20a2e6651500e45d8b12520395f9bb26f12
Jeffrey Chan <jeffschan97@gmail.com>
2017-12-26 11:59:21 -0800

    Mailing list feature for admin panel

--- ideahacks/routes/admin/admin.js
+++ ideahacks/routes/admin/admin.js
@@ -1,3 +1,4 @@
+const User = require('../../db').User
 const Feedback = require('../../db').Feedback
 const formatDate = require('../../helpers').formatters.formatDate
 
@@ -5,11 +6,36 @@ const getAdmin = (req, res) => {
   Feedback.find()
     .populate('user')
     .then(feedback => {
-      for (let f of feedback) {
-        f.formattedTimestamp = formatDate(f.timestamp)
-      }
+      User.find({ hasApplication: true, isVerified: true }).then(users => {
+        // Compile lists of emails for accepted, pending, and rejected users
+        let emailLists = users.reduce(
+          (lists, user, i) => {
+            lists[user.applicationStatus].list.push(user.email)
+            return lists
+          },
+          {
+            accepted: { name: 'Accepted', list: [] },
+            pending: { name: 'Pending', list: [] },
+            rejected: { name: 'Rejected', list: [] }
+          }
+        )
 
-      return res.render('admin', { feedback: feedback.reverse() })
+        let mailingLists = []
+        for (let k in emailLists) {
+          emailLists[k].list = emailLists[k].list.join(', ')
+          mailingLists.push(emailLists[k])
+        }
+
+        // Format the timestamps of Feedback objects
+        for (let f of feedback) {
+          f.formattedTimestamp = formatDate(f.timestamp)
+        }
+
+        return res.render('admin', {
+          feedback: feedback.reverse(),
+          mailingLists
+        })
+      })
     })
 }
 
