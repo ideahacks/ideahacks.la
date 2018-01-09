cb32ee6eb0d3236ee54d1944b7dd0324e6e87754
Jeffrey Chan <jeffschan97@gmail.com>
2018-01-08 22:26:45 -0800

    Load parts script

---
+++ ideahacks/db/scripts/LoadParts.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const filePath = path.join(__dirname, 'parts.csv')
+const dbURI = require('../../config').dbURI
+const Part = require('../Part.js')
+
+mongoose.connect(dbURI, {
+  useMongoClient: true
+})
+
+mongoose.Promise = global.Promise
+
+mongoose.connection.on('error', err => {
+  console.log('Mongoose error: ', err)
+})
+
+fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+  if (err) console.log(err)
+
+  let splitData = data.split('\r\n')
+  let skippedFirstLine = false
+  for (let line of splitData) {
+    if (!skippedFirstLine) {
+      skippedFirstLine = true
+      continue
+    }
+
+    let splitLine = line.split(',')
+    let newPart = new Part({
+      partName: splitLine[2],
+      stock: splitLine[3],
+      description: splitLine[1],
+      type: splitLine[4] === 'Consumable' ? 'consumable' : 'must return',
+      manufacturer: splitLine[5],
+      manufacturerPartNumber: splitLine[6],
+      datasheet: splitLine[7]
+    })
+
+    newPart.save()
+  }
+})
