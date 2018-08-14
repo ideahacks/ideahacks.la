d1d262e9ae4513691b32b61a9b66a1527a75abf6
Kathy Daniels <danielskathyd@gmail.com>
2018-08-13 23:39:13 -0700

    Back end

--- ideahacks/routes/main/static.js
+++ ideahacks/routes/main/static.js
@@ -1,20 +1,25 @@
-const teamData = require('../../db').teamData
-const sponsorsData = require('../../db').sponsorsData
+const teamData = require('../../db').teamData;
+const sponsorsData = require('../../db').sponsorsData;
 
 const getMain = (req, res) => {
-  return res.render('index', { sponsorsData })
-}
+  return res.render('index', { sponsorsData });
+};
+
+const getVenue = (req, res) => {
+  return res.render('venue');
+};
 
 const getTeam = (req, res) => {
-  return res.render('team', { teamData })
-}
+  return res.render('team', { teamData });
+};
 
 const getConfirm = (req, res) => {
-  return res.render('confirm')
-}
+  return res.render('confirm');
+};
 
 module.exports = {
   getMain,
+  getVenue,
   getTeam,
   getConfirm
-}
+};
