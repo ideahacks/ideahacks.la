47de7d7f6f9d44db4da634ade87f25e1f83944b0
Kathy Daniels <32717893+danielskathyd@users.noreply.github.com>
2018-10-15 23:53:52 -0700

    Remove Carousel (#263)
    * font sizing on application
    
    * minor css fixes to me page
    
    * navbar spacing
    
    * removing carousel, adding about section
    
    * navbar toggling for about section
    
    * Log message on successful connection to the db
    
    * Format

--- ideahacks/db/index.js
+++ ideahacks/db/index.js
@@ -1,5 +1,12 @@
-const dbURI = require('../config').dbURI
+// Importing the db module will run this file, which will create a
+// persistent database connection
+
 const mongoose = require('mongoose')
+
+const { dbURI } = require('../config')
+
+// successfulConnectionMessage is logged on successful connection to the db
+const successfulConnectionMessage = 'Successfully connected to the database!'
 
 mongoose.connect(
   dbURI,
@@ -8,12 +15,18 @@ mongoose.connect(
   }
 )
 
+// Plug in global promise library
 mongoose.Promise = global.Promise
 
+// On error connecting to the database, log error
 mongoose.connection.on('error', err => {
   console.log('Mongoose error: ', err)
 })
 
+// On successful connection to the db, log success message
+mongoose.connection.once('open', () => console.log(successfulConnectionMessage))
+
+// Export models within this module
 module.exports = {
   User: require('./User.js'),
   Team: require('./Team.js'),
