7f524a866f83785094e8345a68fd69a6f23a26b4
Jeffrey Chan <jeffschan97@gmail.com>
2018-10-15 19:32:23 -0700

    Log message on successful connection to the db

--- ideahacks/db/index.js
+++ ideahacks/db/index.js
@@ -1,5 +1,12 @@
-const dbURI = require('../config').dbURI
-const mongoose = require('mongoose')
+// Importing the db module will run this file, which will create a
+// persistent database connection
+
+const mongoose = require("mongoose")
+
+const { dbURI } = require("../config")
+
+// successfulConnectionMessage is logged on successful connection to the db
+const successfulConnectionMessage = "Successfully connected to the database!"
 
 mongoose.connect(
   dbURI,
@@ -8,17 +15,23 @@ mongoose.connect(
   }
 )
 
+// Plug in global promise library
 mongoose.Promise = global.Promise
 
-mongoose.connection.on('error', err => {
-  console.log('Mongoose error: ', err)
+// On error connecting to the database, log error
+mongoose.connection.on("error", err => {
+  console.log("Mongoose error: ", err)
 })
 
+// On successful connection to the db, log success message
+mongoose.connection.once("open", () => console.log(successfulConnectionMessage))
+
+// Export models within this module
 module.exports = {
-  User: require('./User.js'),
-  Team: require('./Team.js'),
-  Part: require('./Part.js'),
-  Feedback: require('./Feedback.js'),
-  teamData: require('./data/team.js'),
-  sponsorsData: require('./data/sponsors.js')
+  User: require("./User.js"),
+  Team: require("./Team.js"),
+  Part: require("./Part.js"),
+  Feedback: require("./Feedback.js"),
+  teamData: require("./data/team.js"),
+  sponsorsData: require("./data/sponsors.js")
 }
