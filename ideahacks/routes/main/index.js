6b273cee5f3321cbd34a6d6af04a87231732ea0e
Kathy Daniels <danielskathyd@gmail.com>
2018-10-23 16:52:14 -0700

    adding full navbar to login and registration

--- ideahacks/routes/main/index.js
+++ ideahacks/routes/main/index.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const mainRouter = express.Router()
-const staticHandlers = require('./static.js')
-const authHandlers = require('./auth.js')
-const setResLocals = require('../../helpers').routeHelpers.setResLocals
-const h = require('../../helpers').authHelpers
+const express = require('express');
+const mainRouter = express.Router();
+const staticHandlers = require('./static.js');
+const authHandlers = require('./auth.js');
+const setResLocals = require('../../helpers').routeHelpers.setResLocals;
+const h = require('../../helpers').authHelpers;
 
-mainRouter.get('/', setResLocals, staticHandlers.getMain)
+mainRouter.get('/', setResLocals, staticHandlers.getMain);
 
-mainRouter.get('/team', setResLocals, staticHandlers.getTeam)
-mainRouter.get('/venue', setResLocals, staticHandlers.getVenue)
+mainRouter.get('/team', setResLocals, staticHandlers.getTeam);
+mainRouter.get('/venue', setResLocals, staticHandlers.getVenue);
 
-mainRouter.get('/extended-faq', setResLocals, staticHandlers.getFAQ)
+mainRouter.get('/extended-faq', setResLocals, staticHandlers.getFAQ);
 
-mainRouter.get('/history', setResLocals, staticHandlers.getHistory)
+mainRouter.get('/history', setResLocals, staticHandlers.getHistory);
 
-mainRouter.get('/login', authHandlers.getLogin)
-mainRouter.post('/login', authHandlers.postLogin)
-mainRouter.post('/login/recoverPassword/:email', authHandlers.recoverPassword)
+mainRouter.get('/login', setResLocals, authHandlers.getLogin);
+mainRouter.post('/login', authHandlers.postLogin);
+mainRouter.post('/login/recoverPassword/:email', authHandlers.recoverPassword);
 
-mainRouter.get('/registration', authHandlers.getRegistration)
-mainRouter.post('/registration', authHandlers.postRegistration)
+mainRouter.get('/registration', setResLocals, authHandlers.getRegistration);
+mainRouter.post('/registration', authHandlers.postRegistration);
 
-mainRouter.get('/confirm', h.isAuthenticated, staticHandlers.getConfirm)
-mainRouter.post('/confirm', h.isAuthenticated, authHandlers.postConfirm)
+mainRouter.get('/confirm', h.isAuthenticated, staticHandlers.getConfirm);
+mainRouter.post('/confirm', h.isAuthenticated, authHandlers.postConfirm);
 
-mainRouter.get('/verify/:hash', authHandlers.getVerify)
+mainRouter.get('/verify/:hash', authHandlers.getVerify);
 
-mainRouter.get('/logout', authHandlers.getLogout)
+mainRouter.get('/logout', authHandlers.getLogout);
 
-module.exports = mainRouter
+module.exports = mainRouter;
