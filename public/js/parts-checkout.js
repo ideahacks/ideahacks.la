2f9bb459da400caf169d3d808c24b39dd60cc947
Kathy Daniels <danielskathyd@gmail.com>
2019-01-04 02:37:57 -0800

    redirection

--- public/js/parts-checkout.js
+++ public/js/parts-checkout.js
@@ -1,23 +1,29 @@
 $(() => {
 	// When checkout is selected, display next step
 	$("#checkout-prompt").click(function() {
+		$(".checkin").hide()
 		$("#barcode-input").show()
 		$(this).hide()
 		$("#team-input").show()
-		$("#checkin-prompt").hide()
 
 		$("#out-button").show()
 	})
 
 	// When checkin is selected, display next step
-	$("#{checkin-prompt}").click(function() {
+	$("#checkin-prompt").click(function() {
+		$(".checkin").hide()
 		$("#barcode-input").show()
 		$("#checkout-prompt").hide()
 		$("#team-input").show()
-		$(this).hide()
 
 		$("#in-button").show()
 	})
 
-	$
+	$("button").click(function() {
+		$(".input-form").html("")
+		$(".input-form").html("<p>Your request was successful! Redirecting...</p>")
+		$(this).hide()
+
+		setTimeout(location.reload.bind(location), 2000)
+	})
 })
