2e88f7acd3ab8072a80678e643080279e5e01723
Kathy Daniels <danielskathyd@gmail.com>
2019-01-03 18:42:45 -0800

    jquery woo

--- public/js/parts-checkout.js
+++ public/js/parts-checkout.js
@@ -1,86 +1,23 @@
 $(() => {
-	let checkoutSocket = io("/admin/parts") // open socket to the server
+	// When checkout is selected, display next step
+	$("#checkout-prompt").click(function() {
+		$("#barcode-input").show()
+		$(this).hide()
+		$("#team-input").show()
+		$("#checkin-prompt").hide()
 
-	let modal = document.getElementById("part-checkout-modal")
-
-	// when application clicked on, inject info into modal, and then display modal
-	$(".parts-list").on("click", "li.part", function() {
-		let partName = $(this)
-			.find(".part-name")
-			.text()
-		let currentStock = $(this)
-			.find(".part-stock")
-			.text()
-			.split(" in Stock")[0]
-
-		$('span[name="partName"]').text(partName)
-		$('span[name="current-stock"]').text(currentStock)
-		$(".parts-checkout-form").attr("id", $(this).attr("id"))
-		$('span[name="part-owners"]').text("")
-		modal.style.display = "block"
-
-		// When modal is launched, set the window's onclick handler
-		// to close the modal
-		window.onclick = e => {
-			if (e.target === modal) {
-				resetFormFields()
-				modal.style.display = "none"
-			}
-		}
-
-		let ownerApiUrl = "/api/parts/owners/" + $(this).attr("id")
-		$.ajax({ url: ownerApiUrl, type: "GET" }).done(res => {
-			if (res.status === "success") {
-				$('span[name="part-owners"]').text(res.owners.join(", "))
-			}
-		})
+		$("#out-button").show()
 	})
 
-	// parts checkout form logic
-	$(".parts-checkout-form").submit(function(e) {
-		e.preventDefault()
+	// When checkin is selected, display next step
+	$("#{checkin-prompt}").click(function() {
+		$("#barcode-input").show()
+		$("#checkout-prompt").hide()
+		$("#team-input").show()
+		$(this).hide()
 
-		let partId = $(this).attr("id")
-		let action = $('input[name="part-checkout-radio"][checked]').val()
-		let quantity = $('input[name="quantity"]').val()
-		let teamNumber = $('input[name="teamNumber"]').val()
-		let apiUrl =
-			"/api/parts/action/" + action + "/part/" + partId + "/quantity/" + quantity + "/teamNumber/" + teamNumber
-
-		$.ajax({ url: apiUrl, type: "POST" }).done(res => {
-			if (res.status === "failure") {
-				$(".parts-checkout-error-message").text(res.message)
-			} else {
-				// asynchronously change part stock on pages
-				changePartStock(partId, res.newStock)
-
-				// Broadcast to other clients that a change has been made
-				checkoutSocket.emit("part transformation", { id: partId, stock: res.newStock })
-
-				// clear out form fields and hide modal
-				resetFormFields()
-				modal.style.display = "none"
-			}
-		})
+		$("#in-button").show()
 	})
 
-	// Listen for "part transformation" from socket and make adjustments to page
-	checkoutSocket.on("part transformation", transformation => {
-		changePartStock(transformation.id, transformation.stock)
-	})
+	$
 })
-
-function changePartStock(partId, newStock) {
-	// finds the part on the page and updates it stock to the newStock
-	$(".part#" + partId)
-		.find(".part-stock")
-		.text(newStock + " in Stock")
-}
-
-function resetFormFields() {
-	// resets the parts checkout form so that it's blank every time a user opens it
-	$('input[name="teamNumber"]').val("")
-	$('input[name="quantity"]').val("1")
-	$(".radio-outer-square").removeClass("active")
-	$(".parts-checkout-error-message").text("")
-}
