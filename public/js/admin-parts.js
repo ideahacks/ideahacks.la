a0684f6cd4891da98864941183ebf5411d0bcb2b
Kathy Daniels <32717893+danielskathyd@users.noreply.github.com>
2019-01-10 20:41:12 -0800

    Adding Parts Inventory to Dashboard (#302)
    * basic outline
    
    * stuff
    
    * font size for filter
    
    * parts shopping interface
    
    * added filter
    
    * modal
    
    * extra
    
    * hover
    
    * bye filter keys
    
    * category filtering
    
    * pics
    
    * image to display
    
    * exact cateogry names
    
    * Update public/js/admin-parts.js
    
    Co-Authored-By: danielskathyd <32717893+danielskathyd@users.noreply.github.com>
    
    * oops
    
    * stylistic changes
    
    * wrapping body to fix navbar
    
    * stuff

---
+++ public/js/admin-parts.js
@@ -0,0 +1,14 @@
+$(() => {
+	$(".part-category").click(function() {
+		let category = $(this).text()
+		for (let part of $(".filter-list").children()) {
+			if ($(part).attr("id") == category) {
+				$(part).fadeIn(0)
+			} else if (category == "ALL") {
+				$(part).show()
+			} else {
+				$(part).fadeOut(0)
+			}
+		}
+	})
+})
--- public/js/filter.js
+++ public/js/filter.js
@@ -6,7 +6,7 @@ function filter() {
 	 */
 
 	let searchBarValue = $("input.filter").val()
-	for (let part of $("ul.filter-list").children()) {
+	for (let part of $(".filter-list").children()) {
 		let keyValue = ""
 		$(part)
 			.find(".filter-key")
@@ -15,9 +15,9 @@ function filter() {
 			})
 		keyValue = keyValue.toUpperCase()
 		if (keyValue.indexOf(searchBarValue.toUpperCase()) > -1) {
-			$(part).fadeIn(300)
+			$(part).fadeIn(0)
 		} else {
-			$(part).fadeOut(300)
+			$(part).fadeOut(0)
 		}
 	}
 }
