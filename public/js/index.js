71bf937eba143ca3f150c9c636f02e2b746b8fc9
Kathy Daniels <danielskathyd@gmail.com>
2019-01-16 12:44:35 -0800

    tasteful styling

--- public/js/index.js
+++ public/js/index.js
@@ -4,12 +4,16 @@ $(() => {
 		if (win.width() <= 990) {
 			$(".schedule-time").addClass("col-xs-offset-2")
 			$(".schedule-time").removeClass("col-xs-4")
+			$(".schedule-time").removeClass("text-center")
+			$(".schedule-time").addClass("text-right")
 			$(".schedule-time").addClass("col-xs-5")
 			$(".event").removeClass("col-xs-4")
 			$(".event").addClass("col-xs-5")
 		} else {
 			$(".schedule-time").removeClass("col-xs-offset-2")
 			$(".schedule-time").addClass("col-xs-4")
+			$(".schedule-time").addClass("text-center")
+			$(".schedule-time").removeClass("text-right")
 			$(".schedule-time").removeClass("col-xs-5")
 			$(".event").addClass("col-xs-4")
 			$(".event").removeClass("col-xs-5")
@@ -18,12 +22,16 @@ $(() => {
 	if (win.width() <= 990) {
 		$(".schedule-time").addClass("col-xs-offset-2")
 		$(".schedule-time").removeClass("col-xs-4")
+		$(".schedule-time").removeClass("text-center")
+		$(".schedule-time").addClass("text-right")
 		$(".schedule-time").addClass("col-xs-5")
 		$(".event").removeClass("col-xs-4")
 		$(".event").addClass("col-xs-5")
 	} else {
 		$(".schedule-time").removeClass("col-xs-offset-2")
 		$(".schedule-time").addClass("col-xs-4")
+		$(".schedule-time").addClass("text-center")
+		$(".schedule-time").removeClass("text-right")
 		$(".schedule-time").removeClass("col-xs-5")
 		$(".event").addClass("col-xs-4")
 		$(".event").removeClass("col-xs-5")
