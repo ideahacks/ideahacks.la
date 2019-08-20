c54192a7c4092cadcac05d7698d984649e0a6709
nikki <nikki.k.woo@gmail.com>
2019-08-20 10:38:29 -0700

    Front end changes.

--- public/js/dashboard-application.js
+++ public/js/dashboard-application.js
@@ -18,9 +18,14 @@ function getAndSendApplicationData(toggleHasApplication) {
 		firstName: $('input[name="firstName"]').val(),
 		lastName: $('input[name="lastName"]').val(),
 		phone: $('input[name="phone"]').val(),
-		school: $('input[name="school"]').val(),
+		school: $('input[name="school"]')
+			.find(":selected")
+			.text(),
+		school_other: $('input[name="school_other"]').val(),
 		major: $('input[name="major"]').val(),
-		year: $('input[name="year"]').val(),
+		year: $('input[name="year"]')
+			.find(":selected")
+			.text(),
 		github: $('input[name="github"]').val(),
 		linkedin: $('input[name="linkedin"]').val(),
 		resume: $('input[name="resume"]').val(),
@@ -30,11 +35,20 @@ function getAndSendApplicationData(toggleHasApplication) {
 		teammates: teammateEmails,
 		foodRestrictions: $('input[name="foodRestrictions"]').val(),
 		skillsAndExperience: $('textarea[name="skillsAndExperience"]').val(),
+		hasHackathonExperience: $('select[name="hasTeam"]')
+			.find(":selected")
+			.text(),
 		pastHackathonExperience: $('textarea[name="pastHackathonExperience"]').val(),
 		reasonForParticipation: $('textarea[name="reasonForParticipation"]').val(),
 		themeIdea: $('textarea[name="themeIdea"]').val(),
 		desiredParts: $('textarea[name="desiredParts"]').val(),
 		shirtSize: $('select[name="shirtSize"]')
+			.find(":selected")
+			.text(),
+		needTravelReimbursement: $('select[name=needsReimbursement]')
+			.find(":selected")
+			.text(),
+		needParking: $('select[name=needsParking]')
 			.find(":selected")
 			.text(),
 		hasApplication: toggleHasApplication
