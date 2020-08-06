dfbd5b5b78bb8995755baf42a2a8317e1b9c8916
Robert Peralta <write.robert9@gmail.com>
2020-08-05 18:06:07 -0700

    Add privacy policy (#374)
    * Initial frontend
    
    * Add privacy page to routes
    
    * Improve mobile styling and swap "i.e." for "e.g."

--- ideahacks/routes/main/index.js
+++ ideahacks/routes/main/index.js
@@ -11,6 +11,8 @@ mainRouter.get("/team", setResLocals, staticHandlers.getTeam)
 
 mainRouter.get("/history", setResLocals, staticHandlers.getHistory)
 
+mainRouter.get("/privacy", setResLocals, staticHandlers.getPrivacy)
+
 mainRouter.get("/login", setResLocals, authHandlers.getLogin)
 mainRouter.post("/login", authHandlers.postLogin)
 mainRouter.post("/login/recoverPassword/:email", authHandlers.recoverPassword)
