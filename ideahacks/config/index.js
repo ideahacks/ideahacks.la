77d1cb0ab907ec2b94ad27ce3ff8985aa68b7f8f
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2020-08-15 13:17:38 -0700

    Update config to pull new config vars

--- ideahacks/config/index.js
+++ ideahacks/config/index.js
@@ -6,7 +6,9 @@ if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test") {
 		dbURI: process.env.dbURI,
 		sessionSecret: process.env.sessionSecret,
 		host: process.env.host,
-		SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
+		SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
 	}
 } else {
 	module.exports = require("./development.json")
