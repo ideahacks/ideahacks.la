45b22d406e3608f52ee88d25fc4f14b1961f30b1
Robert Peralta <write.robert9@gmail.com>
2020-09-01 10:06:42 -0700

    Update navbar to reflect hackathon going remote (#380)
    * Update items in navbar, fix forcing relogin
    
    * Run Prettier
    
    * Add small style changes, remove button from mobile

--- ideahacks/helpers/routeHelpers.js
+++ ideahacks/helpers/routeHelpers.js
@@ -2,7 +2,8 @@ const setResLocals = (req, res, next) => {
 	res.locals = {
 		onDashboard: req.baseUrl === "/dashboard",
 		onAdmin: req.baseUrl === "/admin",
-		onMain: req.baseUrl === ""
+		onMain: req.baseUrl === "",
+		authenticated: req.isAuthenticated()
 	}
 	next()
 }
