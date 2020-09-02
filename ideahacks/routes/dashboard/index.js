6bcb92f9b28e9cc53cd76700c59c5293500e862b
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2020-09-02 14:27:10 -0700

    Remove obselete pages from routes

--- ideahacks/routes/dashboard/index.js
+++ ideahacks/routes/dashboard/index.js
@@ -7,11 +7,10 @@ const setResLocals = require("../../helpers").routeHelpers.setResLocals
 
 dashboardRouter.get("/", setResLocals, h.isVerified, dashboardHandlers.getMe)
 
-// Disabled application routes due to end of application period
-// dashboardRouter.get("/application", setResLocals, h.isVerified, applicationHandlers.getApplication)
-// dashboardRouter.post("/application", setResLocals, h.isVerified, applicationHandlers.postApplication)
+dashboardRouter.get("/application", setResLocals, h.isVerified, applicationHandlers.getApplication)
+dashboardRouter.post("/application", setResLocals, h.isVerified, applicationHandlers.postApplication)
 
-dashboardRouter.get("/parts", setResLocals, h.isVerified, dashboardHandlers.getParts)
+// dashboardRouter.get("/parts", setResLocals, h.isVerified, dashboardHandlers.getParts)
 
 // dashboardRouter.get("/teams", setResLocals, h.isVerified, dashboardHandlers.getTeams)
 
@@ -20,6 +19,6 @@ dashboardRouter.get("/me", setResLocals, h.isVerified, dashboardHandlers.getMe)
 dashboardRouter.post("/me/settings", setResLocals, h.isVerified, dashboardHandlers.postSettings)
 dashboardRouter.get("/me/settings", setResLocals, h.isVerified, dashboardHandlers.getSettings)
 
-dashboardRouter.get("/me/parts", setResLocals, h.isVerified, dashboardHandlers.getMyParts)
+// dashboardRouter.get("/me/parts", setResLocals, h.isVerified, dashboardHandlers.getMyParts)
 
 module.exports = dashboardRouter
