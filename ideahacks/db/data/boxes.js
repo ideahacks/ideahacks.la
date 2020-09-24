328dc7cb9f756e72e399641069035899bf343e88
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2020-09-24 15:44:27 -0700

    Add dummy box data to prevent 500 error

---
+++ ideahacks/db/data/boxes.js
@@ -0,0 +1,66 @@
+module.exports = [
+	{
+		boxName: "Base Parts",
+		subtitle: "Included with Motion Add-on and Audio & Visual Add-on ONLY",
+		color: "#888888",
+		parts: [
+			{
+				partName: "Arduino Nano",
+				quantity: 5,
+				category: "Microcontroller",
+				description: "This is an Arduino Nano. It's very interesting.",
+				datasheetURL: "https://google.com",
+				imageURL:
+					"https://www.twinschip.com/image/cache/catalog/Products%20Twins%20Chip%20Store%202020/DEVELOPER%20BOARDS/Arduino/Arduino%20Nano%20V3.0/Arduino%20Nano%20V3.0%20Twins%20Chip%201-425x425.jpg"
+			}
+		]
+	},
+	{
+		boxName: "Motion Add-on",
+		subtitle: "Includes Base Parts",
+		color: "#E1489A",
+		parts: [
+			{
+				partName: "Arduino Nano",
+				quantity: 5,
+				category: "Microcontroller",
+				description: "This is an Arduino Nano. It's very interesting.",
+				datasheetURL: "https://google.com",
+				imageURL:
+					"https://www.twinschip.com/image/cache/catalog/Products%20Twins%20Chip%20Store%202020/DEVELOPER%20BOARDS/Arduino/Arduino%20Nano%20V3.0/Arduino%20Nano%20V3.0%20Twins%20Chip%201-425x425.jpg"
+			}
+		]
+	},
+	{
+		boxName: "Audio & Visual Add-on",
+		subtitle: "Includes Base Parts",
+		color: "#EFA516",
+		parts: [
+			{
+				partName: "Arduino Nano",
+				quantity: 5,
+				category: "Microcontroller",
+				description: "This is an Arduino Nano. It's very interesting.",
+				datasheetURL: "https://google.com",
+				imageURL:
+					"https://www.twinschip.com/image/cache/catalog/Products%20Twins%20Chip%20Store%202020/DEVELOPER%20BOARDS/Arduino/Arduino%20Nano%20V3.0/Arduino%20Nano%20V3.0%20Twins%20Chip%201-425x425.jpg"
+			}
+		]
+	},
+	{
+		boxName: "TI Box",
+		subtitle: "Does NOT include Base Parts",
+		color: "#50B5DD",
+		parts: [
+			{
+				partName: "Arduino Nano",
+				quantity: 5,
+				category: "Microcontroller",
+				description: "This is an Arduino Nano. It's very interesting.",
+				datasheetURL: "https://google.com",
+				imageURL:
+					"https://www.twinschip.com/image/cache/catalog/Products%20Twins%20Chip%20Store%202020/DEVELOPER%20BOARDS/Arduino/Arduino%20Nano%20V3.0/Arduino%20Nano%20V3.0%20Twins%20Chip%201-425x425.jpg"
+			}
+		]
+	}
+]
