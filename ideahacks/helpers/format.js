1bca40c8c16ddc4ba0948dc66dcfa57d002ca902
James King <30397736+jcking1034@users.noreply.github.com>
2020-10-07 15:22:37 -0700

    James/application review modal (#387)
    * Increase border radius on modals and several application review objects to 10px
    
    * In progress of Updating styling of application form
    
    * General style updates, based on Figma
    
    * Improve styling by updating font, font size, color, etc
    
    * Update image, text, styling
    
    * Update styling, questions, and image
    
    * Update headers, styling, images, and questions
    
    * Pull from development
    
    * Update getandSendApplicationData
    
    * Update text color
    
    * Update text color
    
    * Update text color
    
    * Improve styling of application modal on mobile
    
    * Code formatting
    
    * fix sizing on main title on mobile
    
    * Update Schema
    
    * update user schema
    
    * Update Application Submission backend
    
    * Update application modal
    
    * Update padding to improve spacing inconsistency
    
    * make code bewtter
    
    * Address minor feedback about vw font size, exposed routes, and missing colons
    
    * Address comments about general issues concerning icons and positioning
    
    * Improve spacing on mobile
    
    * Contact information format improvements, radio button attempted solution
    
    * code formatting
    
    * Improve radio button mobile design
    
    * Improve stats box mobile styling
    
    * Run Prettier
    
    * Address various spacing issues
    
    * Run Prettier
    
    * Make last style tweaks
    
    * Run Prettier
    
    Co-authored-by: Justin Jianto <justinjianto@gmail.com>
    Co-authored-by: robertnperalta <47515219+robertnperalta@users.noreply.github.com>

--- ideahacks/helpers/format.js
+++ ideahacks/helpers/format.js
@@ -35,6 +35,41 @@ const formatUser = user => {
 			break
 	}
 
+	//FOR KITS//
+	switch (user.kitPreferenceOne) {
+		case "Motion Add-on":
+			user.pref1MotionAddOn = true
+			break
+		case "Audio + Visual Add-on":
+			user.pref1AVAddOn = true
+			break
+		case "TI Box":
+			user.pref1TIBox = true
+			break
+	}
+	switch (user.kitPreferenceTwo) {
+		case "Motion Add-on":
+			user.pref2MotionAddOn = true
+			break
+		case "Audio + Visual Add-on":
+			user.pref2AVAddOn = true
+			break
+		case "TI Box":
+			user.pref2TIBox = true
+			break
+	}
+	switch (user.kitPreferenceThree) {
+		case "Motion Add-on":
+			user.pref3MotionAddOn = true
+			break
+		case "Audio + Visual Add-on":
+			user.pref3AVAddOn = true
+			break
+		case "TI Box":
+			user.pref3TIBox = true
+			break
+	}
+
 	return user
 }
 
