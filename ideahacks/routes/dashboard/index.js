44025e9af963006e79bfc3d83aa05e55d1122148
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2021-06-13 08:16:42 -0700

    Re-enable all routes

--- ideahacks/routes/dashboard/index.js
+++ ideahacks/routes/dashboard/index.js
@@ -9,19 +9,19 @@ const setResLocals = require("../../helpers").routeHelpers.setResLocals
 dashboardRouter.get("/", setResLocals, h.isVerified, dashboardHandlers.getMe)
 
 // APPLICATIONS ARE CLOSED
-// dashboardRouter.get("/application", setResLocals, h.isVerified, applicationHandlers.getApplication)
-// dashboardRouter.post("/application", setResLocals, h.isVerified, applicationHandlers.postApplication)
+dashboardRouter.get("/application", setResLocals, h.isVerified, applicationHandlers.getApplication)
+dashboardRouter.post("/application", setResLocals, h.isVerified, applicationHandlers.postApplication)
 
-// dashboardRouter.get("/parts", setResLocals, h.isVerified, dashboardHandlers.getParts)
+dashboardRouter.get("/parts", setResLocals, h.isVerified, dashboardHandlers.getParts)
 
-// dashboardRouter.get("/teams", setResLocals, h.isVerified, dashboardHandlers.getTeams)
+dashboardRouter.get("/teams", setResLocals, h.isVerified, dashboardHandlers.getTeams)
 
 dashboardRouter.get("/me", setResLocals, h.isVerified, dashboardHandlers.getMe)
 
 dashboardRouter.post("/me/settings", setResLocals, h.isVerified, dashboardHandlers.postSettings)
 dashboardRouter.get("/me/settings", setResLocals, h.isVerified, dashboardHandlers.getSettings)
 
-// dashboardRouter.get("/me/parts", setResLocals, h.isVerified, dashboardHandlers.getMyParts)
+dashboardRouter.get("/me/parts", setResLocals, h.isVerified, dashboardHandlers.getMyParts)
 
 dashboardRouter.get("/resources", setResLocals, staticHandlers.getResources)
 
