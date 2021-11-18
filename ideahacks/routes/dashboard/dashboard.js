a4a46499c09586db8ef35b6974a1673a6a4c39b9
yolanda10202 <yolanda10202@gmail.com>
2021-11-18 11:23:49 -0800

    Tried to fix comments from previous pr. Still have problems with status-container.

--- ideahacks/routes/dashboard/dashboard.js
+++ ideahacks/routes/dashboard/dashboard.js
@@ -12,24 +12,30 @@ const getParts = (req, res) => {
 
 const getMe = (req, res) => {
 	let statusColor = ""
+	let statusDescription = ""
 	switch (req.user.applicationStatus) {
 		case "pending":
-			statusColor = "#50b5dd"
+			statusColor = "#78b0c0"
+			statusDescription = "Your application is under review!"
 			break
 		case "accepted":
-			statusColor = "#acddc9"
+			statusColor = "#619b83"
+			statusDescription = "Welcome to IDEA Hacks!"
 			break
 		case "waitlisted":
-			statusColor = "#e8cc83"
+			statusColor = "#ccab45"
+			statusDescription = "We’ll be in touch if a spot opens up!"
 			break
 		case "rejected":
-			statusColor = "#eab664"
+			statusColor = "#c86b6b"
+			statusDescription = "We’re sorry we didn’t have room"
 			break
 	}
 	res.render("me", {
 		user: req.user,
 		accepted: req.user.applicationStatus === "accepted",
-		statusColor: statusColor
+		statusColor: statusColor,
+		statusDescription: statusDescription
 	})
 }
 
