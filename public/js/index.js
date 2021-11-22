ef02637b53562a5e2b94196151a15fd30ae033bc
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2021-11-21 17:09:59 -0800

    Give schedule location more room, clean JS

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,8 @@
 $(() => {
-	var win = $(window) //this = window
-	$(window).resize(function() {
-		if (win.width() <= 990) {
+	let scheduleResize = function() {
+		if (window.outerWidth <= 990) {
 			$(".schedule-time").addClass("col-xs-offset-2")
-			$(".schedule-time").removeClass("col-xs-4")
+			$(".schedule-time").removeClass("col-xs-3")
 			$(".schedule-time").removeClass("text-center")
 			$(".schedule-time").addClass("text-right")
 			$(".schedule-time").addClass("col-xs-5")
@@ -11,29 +10,15 @@ $(() => {
 			$(".event").addClass("col-xs-5")
 		} else {
 			$(".schedule-time").removeClass("col-xs-offset-2")
-			$(".schedule-time").addClass("col-xs-4")
+			$(".schedule-time").addClass("col-xs-3")
 			$(".schedule-time").addClass("text-center")
 			$(".schedule-time").removeClass("text-right")
 			$(".schedule-time").removeClass("col-xs-5")
 			$(".event").addClass("col-xs-4")
 			$(".event").removeClass("col-xs-5")
 		}
-	})
-	if (win.width() <= 990) {
-		$(".schedule-time").addClass("col-xs-offset-2")
-		$(".schedule-time").removeClass("col-xs-4")
-		$(".schedule-time").removeClass("text-center")
-		$(".schedule-time").addClass("text-right")
-		$(".schedule-time").addClass("col-xs-5")
-		$(".event").removeClass("col-xs-4")
-		$(".event").addClass("col-xs-5")
-	} else {
-		$(".schedule-time").removeClass("col-xs-offset-2")
-		$(".schedule-time").addClass("col-xs-4")
-		$(".schedule-time").addClass("text-center")
-		$(".schedule-time").removeClass("text-right")
-		$(".schedule-time").removeClass("col-xs-5")
-		$(".event").addClass("col-xs-4")
-		$(".event").removeClass("col-xs-5")
 	}
+	$(window).resize(scheduleResize)
+	// Call resize function to catch loading into a resized window
+	scheduleResize()
 })
