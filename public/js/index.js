e55650af9b07f690d50ed0b5f5e14a8faeb2cd25
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2021-11-21 17:15:47 -0800

    Improve schedule styles on mobile

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,19 @@
 $(() => {
 	let scheduleResize = function() {
 		if (window.outerWidth <= 990) {
-			$(".schedule-time").addClass("col-xs-offset-2")
 			$(".schedule-time").removeClass("col-xs-3")
 			$(".schedule-time").removeClass("text-center")
 			$(".schedule-time").addClass("text-right")
-			$(".schedule-time").addClass("col-xs-5")
+			$(".schedule-time").addClass("col-xs-6")
 			$(".event").removeClass("col-xs-4")
-			$(".event").addClass("col-xs-5")
+			$(".event").addClass("col-xs-6")
 		} else {
-			$(".schedule-time").removeClass("col-xs-offset-2")
 			$(".schedule-time").addClass("col-xs-3")
 			$(".schedule-time").addClass("text-center")
 			$(".schedule-time").removeClass("text-right")
-			$(".schedule-time").removeClass("col-xs-5")
+			$(".schedule-time").removeClass("col-xs-6")
 			$(".event").addClass("col-xs-4")
-			$(".event").removeClass("col-xs-5")
+			$(".event").removeClass("col-xs-6")
 		}
 	}
 	$(window).resize(scheduleResize)
