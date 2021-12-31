81d63dd9dcebe0b53b10be503555d953efd02ab1
Grace Ma <glm1550@ucla.edu>
2021-12-30 16:17:23 -0800

    myteam except header

--- ideahacks/routes/dashboard/dashboard.js
+++ ideahacks/routes/dashboard/dashboard.js
@@ -105,59 +105,49 @@ const getMyParts = (req, res) => {
 	})
 }
 
+
 const getMyTeam = (req, res) => {
-	var team = []
+	var team =[]
 
 	let teammates_emails = req.user.teammates
 
-	// teammates_emails.push("robertnperalta@g.ucla.edu")
 	let tlen = teammates_emails.length
 
 	var promises = []
 
-	for (let i = 0; i < tlen; i++) {
+	for (let i = 0; i<tlen; i++){
+
 		let email = teammates_emails[i]
 
-		let teammate = User.find({ email: email })
-
-		// .then(teammate => {
-		// 	let name = teammate[0]._doc.firstName + teammate[0]._doc.lastName
-		// 	let major = teammate[0]._doc.major
-		// 	let year = teammate[0]._doc.year
-		// 	let email = teammate[0]._doc.email
-
-		// 	// res.render("dashboard-my-team", {name, email,major,year})
-		// 	team.push({name, email,major,year})
-		// 	console.log({name, email,major,year})
-
-		// })
+		let teammate = User.find({email: email})
 
 		promises.push(teammate)
 		// console.log(typeof teammate)
 	}
 
 	Promise.all(promises).then(values => {
-		for (let i = 0; i < tlen; i++) {
+		for (let i = 0; i<tlen; i++){
 			let teammate = values[i]
 
-			let name = teammate[0]._doc.firstName + teammate[0]._doc.lastName
+			let name = teammate[0]._doc.firstName + ' ' + teammate[0]._doc.lastName
 			let email = teammate[0]._doc.email
 			let major = teammate[0]._doc.major
 			let year = teammate[0]._doc.year
 
-			console.log(name, email, major, year)
+			console.log(name,email,major,year)
 
-			team.push({ name, email, major, year })
+			team.push({name, email,major,year})
 		}
 
-		console.log("team", team)
+		console.log('team', team)
 
-		res.render("dashboard-my-team", { team })
+		res.render("dashboard-my-team", {team})
 
 		// console.log(values)
 	})
 
-	// res.render("dashboard-my-team", {team})
+
+
 }
 
 module.exports = {
