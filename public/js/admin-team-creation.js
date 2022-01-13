4d7cd6417015462cd3e657250842f766a0eaeb6b
Robert Peralta <write.robert9@gmail.com>
2022-01-13 14:21:13 -0800

    Improve db consistency on team creation page (#449)
    * Remove commented code
    
    * Update teammate emails for each user
    
    * Update JS to only do DB changes at end
    
    * Run Prettier
    
    * Ask for confirmation before moving a user from a team
    
    * Remove debug prints
    
    * Add missing let
    
    * Properly catch non-user emails
    
    * Run Prettier
    
    * Remove reference to user being in their old team
    
    * Fix removing user from old team
    
    * Re-enable page reload on success
    
    * Run Prettier
    
    * Fix misuse of splice()

--- public/js/admin-team-creation.js
+++ public/js/admin-team-creation.js
@@ -1,6 +1,9 @@
+let teamNumber
+let members
+let withTeam
+let withTeamNumbers
 $(() => {
-	let teamNumber
-	$("#team-submit").submit(function(event) {
+	$("#team-number").submit(function(event) {
 		event.stopPropagation()
 		teamNumber = $('input[name="team-number"]').val()
 		if (isNaN(teamNumber)) {
@@ -13,101 +16,139 @@ $(() => {
 			.then(team => {
 				return errorHandler("Team number already exists!")
 			})
-			//Team doesn't exist yet
 			.catch(() => {
-				team = {
-					teamNumber: teamNumber
-				}
-				//Creates team
-				$.ajax({ url: "/api/teams", type: "POST", data: team })
-					.then(() => {
-						$("#team-input").hide()
-						$("#users-input").show()
-					})
-					.catch(err => {
-						errorHandler(err, teamNumber)
-					})
+				$("#team-number").hide()
+				$("#members").show()
 			})
 	})
 
-	$("#users-input").submit(function() {
+	$("#members").submit(function() {
 		let memberEmails = $('textarea[name="members"]')
 			.val()
 			.split("\n")
-		var membersProcessed = 0
 
+		let memberPromises = []
 		memberEmails.forEach(email => {
-			$.get("/api/users/" + email)
-				.then(member => {
-					if (member.hasTeam) {
-						return errorHandler("Email " + member.email + " is already associated with a team!", teamNumber)
-					} else {
-						membersProcessed++
-						if (membersProcessed === memberEmails.length) {
-							membersProcessed = 0
-							memberEmails.forEach(email => {
-								//Loop through members to see if each exists
-								$.get("/api/users/" + email)
-									.then(member => {
-										//Check if the member is already part of a team
-										try {
-											member.hasTeam = true
-											member.teamNumber = teamNumber
-											membersProcessed++
-										} catch (err) {
-											return errorHandler(email + " is not a user!", teamNumber)
-										}
-
-										$.ajax({ url: "/api/users/" + email, type: "PUT", data: member }).catch(err => {
-											return errorHandler(err, teamNumber)
-										})
-
-										if (membersProcessed === memberEmails.length) {
-											successHandler()
-										}
-									})
-									.catch(err => {
-										//Email doesn't exist
-										errorHandler(email + " is not a user!", teamNumber)
-									})
-							})
-						}
-					}
-				})
-				.catch(err => {
-					//Email doesn't exist
-					return errorHandler(email + " is not a user!", teamNumber)
-				})
+			memberPromises.push($.get("/api/users/" + email))
 		})
 
-		// $.ajax({ url: "/api/teams", type: "PUT", data: team })
-		// 	.then(() => {
-		// 		//For each member of the team, add the team number to the database
-		// 		memberEmails.forEach(email => {
-		// 			$.get("/api/users/" + email)
-		// 				.then(user => {
-		// 					user.hasTeam = true
-		// 					user.teamNumber = teamNumber
-		// 					$.ajax({ url: "/api/users/" + email, type: "PUT", data: user })
-		// 						.catch(err => {
-		// 							return errorHandler(err)
-		// 						})
-		// 					team.
-		// 				})
-		// 				.catch(err => {
-		// 					return errorHandler(err)
-		// 				})
-		// 		})
-		// 		successHandler();
-		// 	})
-		// 	.catch(err => {
-		// 		errorHandler(err)
-		// 	})
-		// })
+		members = []
+		withTeam = []
+		withTeamNumbers = []
+		let curEmail = ""
+		$.when(...memberPromises)
+			.then(function(...memberResponses) {
+				for (let i = 0; i < memberResponses.length; i++) {
+					member = memberResponses[i][0]
+					curEmail = memberEmails[i]
+					// If the user has a team, save them for confirmation If the
+					// user has a team number of -1, then they had teammates on
+					// their application, but haven't formally made a team yet
+					if (member.hasTeam && member.teamNumber !== -1) {
+						withTeam.push(member.email)
+						withTeamNumbers.push(member.teamNumber)
+					}
+
+					member.hasTeam = true
+					member.teamNumber = teamNumber
+					member.teammates = []
+					memberEmails.forEach(e => {
+						if (e !== member.email) {
+							member.teammates.push(e)
+						}
+					})
+					members.push(member)
+				}
+
+				if (withTeam.length > 0) {
+					$("#members").hide()
+					$("#with-team").html(withTeam.join(", "))
+					$("#confirm").show()
+					// Return without updating users or creating team
+					return
+				}
+
+				return createTeam()
+			})
+			.catch(err => {
+				//Email doesn't exist
+				errorHandler(curEmail + " is not a user!")
+			})
+	})
+
+	$("#confirm").submit(function() {
+		// Remove any users already on teams from their old teammates' team data
+		let oldTeammatePromises = []
+		// Get a list of users on each old team
+		withTeamNumbers.forEach(number => {
+			oldTeammatePromises.push($.get("/api/users/emails?teamNumber=" + number))
+		})
+		$.when(...oldTeammatePromises).then(function(...oldTeammateResponses) {
+			let teammateUpdatePromises = []
+			for (let i = 0; i < oldTeammateResponses.length; i++) {
+				let userEmail = withTeam[i]
+				let oldMembers = oldTeammateResponses[i][0].split(", ")
+				console.log(oldMembers)
+				let userIndex = oldMembers.indexOf(userEmail)
+				let oldTeammates
+				if (userIndex > -1) {
+					oldMembers.splice(userIndex, 1)
+				}
+				oldTeammates = oldMembers
+
+				// Get the old teammate's data and prepare to remove the user
+				// from their teammate list
+				oldTeammates.forEach(teammate => {
+					$.get("/api/users/" + teammate)
+						.then(teammateData => {
+							let toRemove = teammateData.teammates.indexOf(userEmail)
+							if (toRemove > -1) {
+								teammateData.teammates.splice(toRemove, 1)
+							}
+							teammateUpdatePromises.push(
+								$.ajax({
+									url: "/api/users/" + teammateData.email,
+									type: "PUT",
+									data: teammateData
+								})
+							)
+						})
+						.catch(err => errorHandler(err))
+				})
+			}
+
+			$.when(...teammateUpdatePromises)
+				.then(function(...updateResponses) {
+					// At this point, all references to any of the users being in
+					// their old teams have been removed, so we're clear to create
+					// the new team
+					return createTeam()
+				})
+				.catch(err => errorHandler(err))
+		})
 	})
 })
 
-function deleteTeam(teamNumber) {
+function createTeam() {
+	team = {
+		teamNumber: teamNumber
+	}
+
+	$.ajax({ url: "/api/teams", type: "POST", data: team })
+		.then(() => {
+			members.forEach(member => {
+				$.ajax({ url: "/api/users/" + member.email, type: "PUT", data: member }).catch(err => {
+					errorHandler(err)
+				})
+			})
+			return successHandler()
+		})
+		.catch(err => {
+			errorHandler(err)
+		})
+}
+
+function deleteTeam() {
 	$.ajax({
 		url: "/api/teams",
 		type: "DELETE",
@@ -117,15 +158,18 @@ function deleteTeam(teamNumber) {
 
 // Attempts to log the given error as well as deletes the team that was starting
 // to be formed
-function errorHandler(err, teamNumber) {
+function errorHandler(err) {
 	$(".container").html("There was an error processing your request: " + err)
 	$(this).hide()
 
 	if (teamNumber) {
-		deleteTeam(teamNumber)
+		deleteTeam()
 	}
 
 	setTimeout(location.reload.bind(location), 3000)
+
+	// Break out of the script
+	throw new Error(err)
 }
 
 // successHandler is the logic that runs when everything doesn't blow up
