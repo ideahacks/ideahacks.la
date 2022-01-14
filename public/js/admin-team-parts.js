5e5541a42cd69a9e49d317634df040ccb25eccd5
Robert Peralta <write.robert9@gmail.com>
2022-01-14 13:13:57 -0800

    Update Parts list and My Parts pages (#451)

---
+++ public/js/admin-team-parts.js
@@ -0,0 +1,10 @@
+$(() => {
+	$(".team-dropdown").click(function() {
+		$(this)
+			.find(".team-dropdown-content")
+			.toggle()
+		$(this)
+			.find(".downarrow")
+			.toggleClass("flipped")
+	})
+})
