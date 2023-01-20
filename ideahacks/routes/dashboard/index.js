e832628a82b71864a1ad6806ac0bf2621097f137
Paul Zhang <paulz@ucla.edu>
2023-01-20 14:24:05 -0800

    Temporarily unpublish My Parts and My Team
    Crashes the website when accessed.

--- ideahacks/routes/dashboard/index.js
+++ ideahacks/routes/dashboard/index.js
@@ -22,10 +22,10 @@ dashboardRouter.get("/me", setResLocals, h.isVerified, dashboardHandlers.getMe)
 dashboardRouter.post("/me/settings", setResLocals, h.isVerified, dashboardHandlers.postSettings)
 dashboardRouter.get("/me/settings", setResLocals, h.isVerified, dashboardHandlers.getSettings)
 
-dashboardRouter.get("/me/parts", setResLocals, h.isVerified, dashboardHandlers.getMyParts)
+// dashboardRouter.get("/me/parts", setResLocals, h.isVerified, dashboardHandlers.getMyParts)
 
 // route for myTeam:
-dashboardRouter.get("/me/team", setResLocals, h.isVerified, dashboardHandlers.getMyTeam)
+// dashboardRouter.get("/me/team", setResLocals, h.isVerified, dashboardHandlers.getMyTeam)
 
 dashboardRouter.get("/resources", setResLocals, staticHandlers.getResources)
 
